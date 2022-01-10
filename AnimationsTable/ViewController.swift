3cc1d748040296a668f90887172d908e3d331204
Kamil Tyugaev <samilkuaev@gmail.com>
2022-01-10 15:47:44 +0300

    Initial Commit

---
+++ AnimationsTable/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AnimationsTable
+//
+//  Created by IosDeveloper on 10.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
